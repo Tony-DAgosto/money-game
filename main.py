719d30bba78904613a663241d67caeb07c16da44
Tony-DAgosto <tdagosto2@huskers.unl.edu>
2026-06-30 23:03:23 -0500

    Added collision detection to landmarks around the map.

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ WORLD_HEIGHT = 5000
 screen = pygame.display.set_mode((1600,1000), pygame.RESIZABLE)
 clock = pygame.time.Clock()
 player_rect = pygame.Rect((WORLD_WIDTH // 2,WORLD_HEIGHT // 2,65,65))
-test_rect = pygame.Rect((2200, 2200, 100, 100))
 speed = 8
 
 landmark_list = [
@@ -23,7 +22,11 @@ landmark_list = [
     pygame.Rect(3650, 2200, 100, 100),
     pygame.Rect(4400, 900, 100, 100),
     pygame.Rect(1650, 3300, 100, 100),
-    pygame.Rect(2650, 2650, 100, 100), 
+    pygame.Rect(2650, 2650, 100, 100),
+    pygame.Rect(3050, 700, 100, 100),
+    pygame.Rect(4200, 4650, 100, 100),
+    pygame.Rect(3700, 3950, 100, 100),
+    pygame.Rect(4800, 4700, 100, 100)
 ]
 
 pygame.display.set_caption("Money Game")
@@ -46,15 +49,36 @@ while True:
         speed = 4
     else:
         speed = 8
-    if keys[pygame.K_w]:
-        player_rect.y -= speed
+   # HORIZONTAL PLAYER MOVEMENT
     if keys[pygame.K_a]:
         player_rect.x -= speed
-    if keys[pygame.K_s]:
-        player_rect.y += speed
     if keys[pygame.K_d]:
         player_rect.x += speed
 
+    
+
+    # RESOLVE PLYR HORIZ MVMNT
+    for landmark in landmark_list:
+        if player_rect.colliderect(landmark) and (keys[pygame.K_a] or keys[pygame.K_d]):
+            if player_rect.x > landmark.x:
+                player_rect.left = landmark.right
+            if player_rect.x < landmark.x:
+                player_rect.right = landmark.left
+
+    # VERTICAL PLAYER MOVEMENT
+    if keys[pygame.K_w]:
+        player_rect.y -= speed
+    if keys[pygame.K_s]:
+        player_rect.y += speed
+
+    # RESOLVE PLYR VERT MVMNT
+    for landmark in landmark_list:
+        if player_rect.colliderect(landmark) and (keys[pygame.K_w] or keys[pygame.K_s]):
+            if player_rect.y > landmark.y:
+                player_rect.top = landmark.bottom
+            if player_rect.y < landmark.y:
+                player_rect.bottom = landmark.top
+            
     player_rect.clamp_ip(pygame.Rect(0,0,WORLD_WIDTH,WORLD_HEIGHT))
 
     camera_x = player_rect.centerx - (screen.get_width() // 2)
@@ -67,12 +91,9 @@ while True:
     # **DRAW PHASE**
     screen.fill((75, 140, 45))
 
-    draw_test_rect = test_rect.move(-camera_x, -camera_y)
-    pygame.draw.rect(screen, 'Red', draw_test_rect)
-
     for landmark in landmark_list:
         draw_landmark = landmark.move(-camera_x, -camera_y)
-        pygame.draw.rect(screen, 'Gray45', draw_landmark)
+        pygame.draw.rect(screen, 'Gray60', draw_landmark)
         
 
     draw_player_rect = player_rect.move(-camera_x, -camera_y)
@@ -83,4 +104,3 @@ while True:
 
     # Tells game loop to only run 60 times per second
     clock.tick(60)
-
